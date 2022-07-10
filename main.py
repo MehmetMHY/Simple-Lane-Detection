61c6fa5b4af2ae3559b6f423e60921a190e3e7c0
mehmet-mhy <mehmet.mhy@gmail.com>
2022-07-10 18:09:29 +0300

    added try except for CTRL-C key press in main.py

--- main.py
+++ main.py
@@ -9,29 +9,31 @@ import cv2
 
 
 if __name__ == "__main__":
+    try:
+        # get user to input their desired whole number of the splits_per_half value
+        user_input = ui.validating_user_input("Please Enter splits_per_half To Continue!",
+                                        "Input desired splits_per_half value (ex: 6): ",
+                                        "Invalid Input, Please Enter Only Whole Numbers!")
 
-    # get user to input their desired whole number of the splits_per_half value
-    user_input = ui.validating_user_input("Please Enter splits_per_half To Continue!",
-                                       "Input desired splits_per_half value (ex: 6): ",
-                                       "Invalid Input, Please Enter Only Whole Numbers!")
+        # set number of divides per each half (right & left) of the image    
+        splits_per_half = user_input
 
-    # set number of divides per each half (right & left) of the image    
-    splits_per_half = user_input
+        # selected video name/path
+        video = ui.select_video("./assets/")   # videos.toronto_way
 
-    # selected video name/path
-    video = ui.select_video("./assets/")   # videos.toronto_way
-
-    if video is not None:
-        # apply clasic lane detection:
-        sm.classic_lane_detection(video, splits_per_half)
-        
-        # closing message
-        print("\n" + "[ Enter SPACE To Exit ]")
-        cv2.waitKey(0)
-        
-    else:
-        print("Failed to load, check inputed splits_per_half value or inputed video!")
-
+        if video is not None:
+            # apply clasic lane detection:
+            sm.classic_lane_detection(video, splits_per_half)
+            
+            # closing message
+            print("\n" + "[ Enter SPACE To Exit ]")
+            cv2.waitKey(0)
+            
+        else:
+            print("Failed to load, check inputed splits_per_half value or inputed video!")
+    except KeyboardInterrupt:
+        print("\n" + "Closed program due to interrupt caused by the pressing of CTRL-C")
+        pass
 
 
 
